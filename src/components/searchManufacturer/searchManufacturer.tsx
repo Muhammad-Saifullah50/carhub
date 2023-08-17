21446f8baed9b3aab1a0d7b3c98d8de0113ea6ff
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-08-17 19:07:27 +0500

    cretaed filtrer

--- src/components/searchManufacturer/searchManufacturer.tsx
+++ src/components/searchManufacturer/searchManufacturer.tsx
@@ -1,14 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { SearchManufacturerProps } from "@/types";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
+import { manufacturers } from "@/constants";
+import { relative } from "path";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  const [query, setquery] = useState("")
+  const [query, setquery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+        item
+          .toLowerCase()
+          .replace(/\s+/g, "")
+          .includes(query.toLowerCase().replace(/\s+/g, ""))
+      );
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -28,6 +40,26 @@ const SearchManufacturer = ({
             displayValue={(manufacturer: string) => manufacturer}
             onChange={(e) => setquery(e.target.value)}
           />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setquery("")}
+          >
+            <Combobox.Options>
+              {(filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) =>
+                    `relative search-manufacturer__option ${active ? "bg-primary-blue text-white" : "text-gray-900"}`}
+                  value={item}>
+                  {item}
+                </Combobox.Option>
+              ))
+              )}
+            </Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
     </div>
