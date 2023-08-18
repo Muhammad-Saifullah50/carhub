e4ecb898e7d85cab7389f0afd755a6204fe7ffcc
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-08-18 14:50:16 +0500

    resolved keys error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,9 @@
 export async function fetchCars() {
+    const key = process.env.XRapidAPIKey!
+    const host = process.env.XRapidAPIHost!
     const headers = {
-        'X-RapidAPI-Key': 'acc1c793c2msh78170ef1d048f5ep187954jsn65c8b28d5f64',
-        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+        'X-RapidAPI-Key': key,
+        'X-RapidAPI-Host': host
     }
     const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
         headers: headers
@@ -17,13 +19,13 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
     const mileageFactor = 0.1; // Additional rate per mile driven
     const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
+
     // Calculate additional rate based on mileage and age
     const mileageRate = city_mpg * mileageFactor;
     const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
+
     // Calculate total rental rate per day
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
+
     return rentalRatePerDay.toFixed(0);
-  };
+};
